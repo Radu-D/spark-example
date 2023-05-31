606bc8dd3dc7fa33e8923b63b1e4232f99e7ac91
Radu Dumitru <raduoctav31@protonmail.com>
2023-05-30 21:53:14 -0400

    feat: Init

--- Program.cs
+++ Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using Microsoft.Spark;
+using Microsoft.Spark.Sql;
 
-Console.WriteLine("Hello, World!");
+//using Microsoft.Spark;
+
+namespace SparkExample
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var spark = SparkSession
+                .Builder()
+                //.Master("local[*]")
+                // .Master("192.168.2.195:49571")
+                .Master("spark://ca1df3e51fb2:7077")
+                .AppName("connectors")
+                .Config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+                .Config("spark.eventLog.enabled", "true")
+                .Config("spark.eventLog.dir", "/home/iceberg/spark-events")
+                .Config("spark.history.fs.logDirectory", "/home/iceberg/spark-events")
+                .GetOrCreate();
+
+            spark.SparkContext.SetLogLevel("ERROR");
+            
+            // Mock data from https://mockaroo.com/
+            var df = spark.Read().Json("facilities.json");
+            df.Show();
+            df.Write().Parquet("nom_fichier00.parquet");
+            
+            // var spark = SparkSession.Builder().AppName("Connectors").Config(new SparkConf()).Master("spark://ca1df3e51fb2:7077").GetOrCreate();
+            // var df = spark.Read().Json("brunetPharmacies.json");
+            // df.Show();
+            //
+            // var t = "";
+            //var df = spark.CreateDataFrame();
+            //df.Show();
+            //df.Write().Parquet("nom_fichier.parquet");
+        }
+    }
+}
