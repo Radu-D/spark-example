5627c91a2ec48068c3b17ebef874b9ae269c34c8
Radu Dumitru <raduoctav31@protonmail.com>
2023-06-27 22:33:41 -0400

    YES

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Apache.Arrow.Types;
+﻿using System.Diagnostics;
+using Apache.Arrow.Types;
 using Microsoft.Spark;
 using Microsoft.Spark.Sql;
 using Microsoft.Spark.Sql.Streaming;
@@ -13,32 +14,61 @@ namespace SparkExample
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            MakeABrunetTableUsingSpark();
-            //TrinoInsert();
+            // if (Environment.GetEnvironmentVariable("DOTNET_WORKER_DEBUG")?.Equals("1") == true)
+            // {
+            Debugger.Launch();
+            //}
             
+            try
+            {
+                GetEachTableFromResourcesBronzeIceberg();
+                //await TrinoSelect();
+                //MakeABrunetTableUsingSpark();
+                //TrinoInsert();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+        }
+
+        private static void GetEachTableFromResourcesBronzeIceberg()
+        {
+            var sparkConf = new SparkConf();
+
+            var spark = SparkSession
+                .Builder()
+                .Master("local[*]")
+                .AppName("connectors")
+                .Config(sparkConf)
+                .EnableHiveSupport()
+                .GetOrCreate();
+
+            spark.SparkContext.SetLogLevel("ERROR");
+
+            var allTablesFromResourcesBronze = spark.Sql("SHOW TABLES FROM resources_bronze");
+            var tableNames = allTablesFromResourcesBronze.Select("tableName").As("Table");
+            tableNames.Show();
+            
+            // Get first column (the table name) from each returned row
+            var tableNamesAsStrings = tableNames.Collect().Select(x => x.Values.First());
+            foreach (var tableName in tableNamesAsStrings)
+            {
+                var entriesFromResourcesBronze = spark.Sql($"SELECT * FROM resources_bronze.{tableName}");
+                entriesFromResourcesBronze.Show();
+                entriesFromResourcesBronze.Write()
+                    .Mode("append")
+                    .Json($"./resources_bronze.{tableName}.parquet");
+            }
         }
 
         private static void MakeABrunetTableUsingSpark()
         {
             var sparkConf = new SparkConf();
-            
-            // data_sor config
-            sparkConf.Set("spark.master", "local[*]");
-            sparkConf.Set("spark.driver.memory", "30g");
-            sparkConf.Set("spark.driver.host", "127.0.0.1");
-            sparkConf.Set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions");
-            sparkConf.Set("spark.sql.defaultCatalog", "lakehouse");
-            sparkConf.Set("spark.sql.catalog.lakehouse", "org.apache.iceberg.spark.SparkCatalog");
-            sparkConf.Set("spark.sql.catalog.lakehouse.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog");
-            sparkConf.Set("spark.sql.catalog.lakehouse.io-impl", "org.apache.iceberg.aws.s3.S3FileIO");
-            sparkConf.Set("spark.sql.catalog.lakehouse.warehouse", "s3://clinia-data-lake");
-            sparkConf.Set("spark.hadoop.fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem");
-            sparkConf.Set("spark.hadoop.google.cloud.auth.service.account.enable", "true");
-            sparkConf.Set("spark.hadoop.google.cloud.auth.service.account.json.keyfile", "/Users/radud/Documents/Clinia/SparkExample/key.json");//"../../../key.json");
-            sparkConf.Set("spark.sql.caseSensitive", "true");
-            sparkConf.Set("spark.driver.extraClassPath", "./");
 
             var spark = SparkSession
                 .Builder()
@@ -64,6 +94,48 @@ namespace SparkExample
                 .Mode("overwrite")
                 .Format("iceberg")
                 .SaveAsTable("resources_bronze_test.brunetv2");
+        }
+
+        private static async Task TrinoSelect()
+        {
+            #region Trino
+
+            var config = new TrinoClientSessionConfig
+            {
+                Host = "localhost",
+                Port = 8080,
+                Catalog = "iceberg",
+                //Schema = "pharmacy_brunet"
+            };
+            
+            var client = new TrinodbClient(config);
+            var request = new ExecuteQueryV1Request("SELECT * FROM resources_bronze.pharmacy_brunet");
+            var queryResponse = await client.ExecuteQueryV1(request);
+
+            var dataToJson = queryResponse.DataToJson();
+
+            var columns = queryResponse.Columns.ToList();
+            var listOfRows = queryResponse.Data.ToList();
+
+            foreach (var rows in listOfRows)
+            {
+                foreach (var column in columns)
+                {
+                    switch (column.Type)
+                    {
+                        case "varchar":
+                            Console.WriteLine("string");
+                            break;
+                        default:
+                            Console.WriteLine(column.Type);
+                            break;
+                    }
+
+                    var element = rows.ElementAt(columns.IndexOf(column));
+                    Console.WriteLine($"[{column.Name}] - {element.ToString()}");
+                }
+            }
+            #endregion
         }
 
         private static void TrinoInsert()
